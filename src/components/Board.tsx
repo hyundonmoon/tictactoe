59b80d6bde0e273b319330f288673a56509cc802
Hyun Don Moon <hyundonmoon@gmail.com>
2024-08-19 00:32:09 +0900

    create circle, x-mark icon components

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,6 @@
 import { BoardState as BoardModel } from '../types/boardState.model';
+import CircleIcon from './CircleIcon';
+import XMarkIcon from './XMarkIcon';
 
 export default function Board({
   boardState,
@@ -12,10 +14,10 @@ export default function Board({
       {boardState.map((value, index) => (
         <button
           key={index}
-          className="col-span-1 w-36 h-36 p-8 bg-blue-100 shadow-md shadow-blue-400/50 cursor-pointer rounded-3xl flex justify-center items-center overflow-hidden active:shadow-inner"
+          className="col-span-1 w-36 h-36 p-8 bg-amber-50 shadow-md shadow-amber-200/50 cursor-pointer rounded-3xl flex justify-center items-center overflow-hidden active:shadow-inner"
           onClick={() => handleBoardClick(index)}
         >
-          {value}
+          {value === 'O' ? <CircleIcon /> : value === 'X' ? <XMarkIcon /> : ''}
         </button>
       ))}
     </div>
