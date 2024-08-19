1932a6b69b36ca314668ade0af3b888a19fe6ff5
Hyun Don Moon <hyundonmoon@gmail.com>
2024-08-19 22:40:48 +0900

    fix styles

--- src/components/SinglePlayerScoreboard.tsx
+++ src/components/SinglePlayerScoreboard.tsx
@@ -6,9 +6,9 @@ export default function SinglePlayerScoreboard({
   stats: GameStats;
 }) {
   return (
-    <div className="flex justify-center items-center gap-8 font-bold text-gray-800 text-xl shadow-sm">
+    <div className="flex justify-center items-center gap-8 font-bold text-gray-800 text-xl">
       <p className="tracking-widest">YOU</p>
-      <div className="flex-1 bg-gray-50 px-4 py-2 shadow-md rounded-3xl">
+      <div className="flex-1 bg-gray-50 px-4 py-2 rounded-3xl shadow-sm">
         {stats.wins + stats.draws} - {stats.losses + stats.draws}
       </div>
       <p className="tracking-widest">AI</p>
