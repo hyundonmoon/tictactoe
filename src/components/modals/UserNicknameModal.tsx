acf70dde7ef76b2994f867a550cf7d9c141859a3
Hyun Don Moon <hyundonmoon@gmail.com>
2024-08-23 15:01:27 +0900

    implement user nickname modal

---
+++ src/components/modals/UserNicknameModal.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function UserNicknameModal({
+  isOpen,
+  closeModal,
+  onSubmit,
+}: {
+  isOpen: boolean;
+  closeModal: () => void;
+  onSubmit: (nickname: string) => void;
+}) {
+  const ref = useRef<HTMLDialogElement | null>(null);
+  const [nickname, setNickname] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (nickname) {
+      onSubmit(nickname);
+      closeModal();
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('nickname')) {
+      setNickname(localStorage.getItem('nickname') ?? '');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [isOpen]);
+
+  return (
+    <dialog
+      ref={ref}
+      className="p-6 rounded-lg shadow-lg bg-white text-center max-w-md w-11/12"
+      onClose={() => {
+        closeModal();
+      }}
+    >
+      <h2 className="text-2xl font-semibold mb-4">Enter Your Nickname</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your nickname"
+          autoFocus
+          required
+          className="p-2 border border-gray-300 rounded w-full mb-4"
+          minLength={3}
+          maxLength={20}
+          value={nickname}
+          onChange={(e) => {
+            setNickname(e.target.value?.trim());
+          }}
+        />
+        <div className="flex justify-center space-x-4">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+          >
+            Confirm
+          </button>
+          <button
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded transition"
+            onClick={closeModal}
+          >
+            Close
+          </button>
+        </div>
+      </form>
+    </dialog>
+  );
+}
