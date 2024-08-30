7bf07cbeb849c1e61df048b69e82f10d6fe99669
Hyun Don Moon <hyundonmoon@gmail.com>
2024-08-30 12:28:01 +0900

    use env variable

--- src/hooks/useRoomList.ts
+++ src/hooks/useRoomList.ts
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { Room } from '../types/socket.model';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function useRoomList(filter: 'waiting' | 'all' = 'all') {
   return useQuery({
     queryKey: ['room-list', filter],
     queryFn: async (): Promise<Room[]> => {
-      const res = await fetch(`http://localhost:3000/rooms?filter=${filter}`);
+      const res = await fetch(`${API_URL}/rooms?filter=${filter}`);
 
       if (!res.ok) {
         throw new Error('error fetching room list');
